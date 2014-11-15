92e18201a7a02c87ada313506a3c52388f5b674d
igorshainsky <igorshainsky@gmail.com>
2014-11-15 09:07:52 +0200

    test2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World from Igor,my rep!')
+  response.send('Hello World from Igor,success!')
 })
 
 app.listen(app.get('port'), function() {
